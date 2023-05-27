225cd96f69ffcc04855279eafe0ad63359f2efd4
louisdpro <122191019+Louisdpr0@users.noreply.github.com>
2023-05-28 00:16:11 +0700

    Add files via upload

---
+++ plugins/creator.js
@@ -0,0 +1,18 @@
+import fetch from 'node-fetch'
+let handler = async (m, { conn, usedPrefix, text, args, command }) => {
+let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
+let pp = await conn.profilePictureUrl(who).catch(_ => hwaifu.getRandom())
+let name = await conn.getName(who)
+
+  const sentMsg = await conn.sendContactArray(m.chat, [
+    [`${nomorown}`, `${await conn.getName(nomorown+'@s.whatsapp.net')}`, `💌 LOVE YOU `],
+    [`${conn.user.jid.split('@')[0]}`, `${await conn.getName(conn.user.jid)}`, `🎈 ʙᴏᴛ ᴡʜᴀᴛsᴀᴘᴘ`]
+  ], fkontak)
+  await m.reply(`CONTACT @${m.sender.split(`@`)[0]} `)
+  } 
+handler.help = ['owner', 'creator']
+handler.tags = ['info']
+
+handler.command = /^(owner|creator)$/i
+
+export default handler
